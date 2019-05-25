5d9402d45fd3e35494953beecbcde882654b3607
Gokul Kulkarni <gokul.kulkarni@yasco.in>
2019-05-25 23:03:04 +0530

    picking image from gallery and showing

--- src/details/DetailsScreen.js
+++ src/details/DetailsScreen.js
@@ -1,20 +1,103 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { Card, Button, Image, Overlay } from "react-native-elements";
+import ImagePicker from 'react-native-image-crop-picker';
 
 class DetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "",
+      isVisible: false
+    };
+  }
+
+  selectImage() {
+    this.setState({
+      isVisible: true
+    })
+  }
+
+  openGallery() {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true
+    }).then(userImageResponse => {
+      const source = `file://${userImageResponse.path}`;
+      this.setState({
+        image: source,
+        isVisible: false
+      })
+    });
+  }
+
+  openCamera() {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(userImageResponse => {
+      const source = `file://${userImageResponse.path}`;
+      this.setState({
+        image: source,
+        isVisible: false
+      })
+    });
+  }
 
   render() {
-    console.log('this is the details being received: ', this.props.navigation.getParam('item'));
-    let item = this.props.navigation.getParam('item');
+    console.log(
+      "this is the details being received: ",
+      this.props.navigation.getParam("item")
+    );
+    let item = this.props.navigation.getParam("item");
     return (
-      <Card
-        title={item.title}
-        image={{
-          uri: item.url
-        }}
-      />
-    )
+      <View>
+        <Card
+          title={item.title}
+          image={{
+            uri: item.url
+          }}
+        />
+        <View>
+          <Button title="Upload"
+            onPress={this.selectImage.bind(this)}
+            style={{
+              margin: 10
+            }}
+          />
+
+          <Image
+            style={{ width: 200, height: 200, alignSelf: 'center' }}
+            source={{ uri: this.state.image }}
+          />
+        </View>
+        {this.state.isVisible && (
+          <Overlay
+            isVisible={this.state.isVisible}
+            onBackdropPress={() => this.setState({ isVisible: false })}
+            height={200}
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'space-around'
+              }}
+            >
+              <Button
+                title='Camera'
+                onPress={this.openCamera.bind(this)}
+              />
+              <Button
+                title='Gallery'
+                onPress={this.openGallery.bind(this)}
+              />
+            </View>
+          </Overlay>
+        )}
+      </View>
+    );
   }
 }
 
